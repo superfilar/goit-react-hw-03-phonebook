2e96a447a50fc5ed57f7a231cfdd31db913ed008
superfilar <mateusz9650@gmail.com>
2024-01-29 19:29:00 +0100

    Hw03 v1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,21 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     this.repeatControl(data);
   };
